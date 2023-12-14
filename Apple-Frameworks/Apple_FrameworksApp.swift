af614c581dc604efe9ddb677bcf3038143d24f22
stevenTheDeve <steventhedeve@gmail.com>
2023-12-14 15:15:34 -0800

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Estevan Hinojosa on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
