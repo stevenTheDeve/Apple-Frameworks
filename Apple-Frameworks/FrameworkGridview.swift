e09bfd47cf1792c5085927949f8d78038f179265
stevenTheDeve <steventhedeve@gmail.com>
2023-12-19 23:26:07 -0800

    Framework Addition

--- Apple-Frameworks/FrameworkGridview.swift
+++ Apple-Frameworks/FrameworkGridview.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct FrameworkGridview: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        VStack {
-            Image("app-clip")
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text("App-Clips")
+        LazyVGrid(columns: columns) {
+            ForEach(MockData.frameworks) { framework in
+                FrameworkTitleView(framework: framework)
+            }
+           
         }
     }
 }
 
 #Preview {
     FrameworkGridview()
+}
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    
+    var body: some View {
+        
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+        }
+    }
 }
